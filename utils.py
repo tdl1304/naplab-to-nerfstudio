6e96ed2385d901fce48d4e304f9d31876a7ac392
Tommy Luu <tommydl@stud.ntnu.no>
2024-05-05 03:44:24 +0200

    testing

--- utils.py
+++ utils.py
@@ -1,55 +1,72 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_3d(series_list: list[list], labels: list[str], figsize=(5, 5)):
-    """Plot 3D coordinates."""
-    fig = plt.figure(figsize=figsize)
-    ax = fig.add_subplot(111, projection='3d')
+def plot_coordinates(series_list: list[list], labels: list[str], figsize=(5, 5), is3D=False):
+    """Plot 2D or 3D coordinates."""
+    if is3D:
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111, projection='3d')
+    else:
+        plt.figure(figsize=figsize)
     colorpallet = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w', 'b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
-    for s in series_list:
-        x = [data[0] for data in s]
-        y = [data[1] for data in s]
-        z = [data[2] for data in s]
-        ax.plot(x, y, z, marker='.', markersize=0.5, color=colorpallet.pop(0))
-        
-    plt.legend(labels)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
-
-
-def plot_2d(series_list: list[list], labels: list[str], figsize=(5, 5)):
-    """Plot 2D coordinates."""
-    colorpallet = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w', 'b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
-    plt.figure(figsize=figsize)
-    for s in series_list:
-        x = [data[0] for data in s]
-        y = [data[1] for data in s]
-        plt.plot(x, y, marker='.', markersize=0.5, color=colorpallet.pop(0))
+    for series in series_list:
+        x = [data[0] for data in series]
+        y = [data[1] for data in series]
+        if is3D:
+            z = [data[2] for data in series]
+            ax.plot(x, y, z, marker='.', markersize=0.5, color=colorpallet.pop(0))
+        else:
+            plt.plot(x, y, marker='.', markersize=0.5, color=colorpallet.pop(0))
     
+    if is3D:
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax.set_title('--------------------------------3D plot of XYZ coordinates--------------------------------')
+    else:
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('--------------------------------2D plot of XY coordinates--------------------------------')
+
     plt.legend(labels)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('2D plot of XY coordinates')
+    plt.tight_layout()
     plt.show()
 
 
-def plot_directions(position_series_list: list[list], direction_vector_list: list[list], labels: list[str], figsize=(5, 5)):
-    """Plot 2D coordinates."""
+def plot_directions(position_series_list: list[list], direction_vector_list: list[list], labels: list[str], figsize=(5, 5), is3D=False):
+    """Plot 2D or 3D coordinates."""
+    if is3D:
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111, projection='3d')
+    else:
+        plt.figure(figsize=figsize)
+
     colorpallet = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w', 'b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
-    plt.figure(figsize=figsize)
+    
     for serie, dirs in zip(position_series_list, direction_vector_list):
         color = colorpallet.pop(0)
         x = [data[0] for data in serie]
         y = [data[1] for data in serie]
+        if is3D:
+            z = [data[2] for data in serie]
         
         V = np.array(dirs)
         
-        if len(V) > 0:
-            plt.quiver(x, y, V[:, 0], V[:, 1], color=color, scale=3, units="xy", width=0.025)
+        if is3D:
+            ax.quiver(x, y, z, V[:, 0], V[:, 1], V[:, 2], color=color, length=1, arrow_length_ratio=.2)
+        else:
+            plt.quiver(x, y, V[:, 0], V[:, 1], color=color, scale=2, units="xy", width=0.025)
 
+    if is3D:
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+    else:
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        
     plt.legend(labels)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('2D plot of XY coordinates')
+    l = "----------------------------------"
+    plt.title(f'{l}3D plot of XYZ coordinates{l}' if is3D else f'{l}2D plot of XY coordinates{l}')
+    plt.tight_layout()
     plt.show()
